4c442806b0c69aa2c171398805fc29ca7e84c0a5
Bruno Kirschner <bruno.kirschner@online.de>
2018-12-30 15:46:40 +0100

    [2018|Rust] Add solution for the second part of 2018/12/07.

--- 2018/aoc07/src/main.rs
+++ 2018/aoc07/src/main.rs
@@ -18,10 +18,16 @@ fn main() -> Result<()> {
     io::stdin().read_to_string(&mut input)?;
 
     let steps = setup_steps(&input)?;
+    let step_order = determine_synchronous_step_order(&steps);
 
-    let step_order = determine_step_order(&steps);
     let step_order = step_order.into_iter().collect::<String>();
     println!("Step order: {}", step_order);
+
+    let steps = setup_steps(&input)?;
+    let (step_order, duration) = calc_async_duration(&steps, 5, 60);
+
+    let step_order = step_order.into_iter().collect::<String>();
+    println!("Async step order: {} in {} seconds", step_order, duration);
 
     Ok(())
 }
@@ -76,35 +82,120 @@ fn setup_steps(input: &str) -> Result<HashMap<char, StepCell>> {
     Ok(steps)
 }
 
-fn determine_step_order(steps: &HashMap<char, StepCell>) -> Vec<char> {
-    let mut steps = steps.iter().map(|(_, step)| step).collect::<Vec<_>>();
+fn determine_synchronous_step_order(steps: &HashMap<char, StepCell>) -> Vec<char> {
+    let mut steps = steps.values().map(StepCell::clone).collect::<Vec<_>>();
     let mut final_order = vec![];
 
     while !steps.is_empty() {
-        steps.sort_by(|lhs, rhs| {
-            let lhs = lhs.borrow();
-            let rhs = rhs.borrow();
-
-            let lhs_len = lhs.depends_on.len();
-            let rhs_len = rhs.depends_on.len();
-
-            let ordering = rhs_len.cmp(&lhs_len);
-            match ordering {
-                Ordering::Equal => rhs.cmp(&lhs),
-                _ => ordering,
-            }
-        });
+        sort_remaining_steps(&mut steps);
 
         if let Some(next_step) = steps.pop() {
-            let parent_ref = next_step.borrow();
-            for child in &parent_ref.parent_for {
-                let mut child_ref = child.borrow_mut();
-                child_ref.depends_on.remove(&next_step);
+            {
+                let parent_ref = next_step.borrow();
+                for child in &parent_ref.parent_for {
+                    let mut child_ref = child.borrow_mut();
+                    child_ref.depends_on.remove(&next_step);
+                }
             }
 
-            final_order.push(parent_ref.identifier);
+            final_order.push(next_step.borrow().identifier);
         }
     }
 
     final_order
+}
+
+fn sort_remaining_steps(steps: &mut [StepCell]) {
+    steps.sort_by(|lhs, rhs| {
+        let lhs = lhs.borrow();
+        let rhs = rhs.borrow();
+
+        let lhs_len = lhs.depends_on.len();
+        let rhs_len = rhs.depends_on.len();
+
+        let ordering = rhs_len.cmp(&lhs_len);
+        match ordering {
+            Ordering::Equal => rhs.cmp(&lhs),
+            _ => ordering,
+        }
+    });
+}
+
+fn calc_async_duration(
+    steps: &HashMap<char, StepCell>,
+    num_worker: usize,
+    base_step_duration: u32,
+) -> (Vec<char>, u32) {
+    let mut worker: Vec<Worker> = vec![Worker::default(); num_worker];
+    let mut steps = steps.values().map(StepCell::clone).collect::<Vec<_>>();
+
+    let mut final_order = vec![];
+    let mut current_time = 0u32;
+
+    sort_remaining_steps(&mut steps);
+
+    loop {
+        let finished = steps.is_empty() && worker.iter().all(|worker| worker.is_idle(current_time));
+        if finished {
+            break;
+        }
+
+        worker
+            .iter_mut()
+            .filter(|worker| worker.is_idle(current_time))
+            .for_each(|worker| {
+                let finished_step = std::mem::replace(&mut worker.step, None);
+                if let Some(finished_step) = finished_step {
+
+                    let parent_ref = finished_step.borrow();
+                    for child in &parent_ref.parent_for {
+                        let mut child_ref = child.borrow_mut();
+                        child_ref.depends_on.remove(&finished_step);
+                    }
+
+                    let identifier = finished_step.borrow().identifier;
+                    final_order.push(identifier);
+
+                    sort_remaining_steps(&mut steps);
+                }
+
+                let step_available = if let Some(next_step) = steps.last() {
+                    next_step.borrow().depends_on.is_empty()
+                } else {
+                    false
+                };
+
+                if step_available {
+                    if let Some(next_step) = steps.pop() {
+                        // Every identifier applies some custom delay.
+                        let identifier = next_step.borrow().identifier;
+                        let finish_time =
+                            current_time + base_step_duration + (identifier as u32 - '@' as u32);
+
+                        worker.start_work(&next_step, finish_time);
+                    }
+                }
+            });
+
+        current_time += 1;
+    }
+
+    (final_order, current_time)
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Hash, Default)]
+struct Worker {
+    done_at: u32,
+    step: Option<StepCell>,
+}
+
+impl Worker {
+    pub fn start_work(&mut self, step: &StepCell, done_at: u32) {
+        self.done_at = done_at;
+        self.step = Some(StepCell::clone(step));
+    }
+
+    pub fn is_idle(&self, current_time: u32) -> bool {
+        self.done_at <= current_time
+    }
 }
