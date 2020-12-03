427d9bc75e3d18c587a51fa8e75a1a1a9ee29eb7
Bruno Kirschner <bruno.kirschner@online.de>
2020-12-03 21:55:59 +0100

    [2020|Rust] Add solution for the first part of day 3.

---
+++ 2020/03/src/main.rs
@@ -0,0 +1,170 @@
+use std::fmt::{Display, Formatter, Write};
+use std::str::FromStr;
+use std::{env, fs};
+
+type Error = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), Error> {
+    let rows = parse_input::<Row>()?;
+    let map = Map(rows);
+
+    let num_trees = map.count_trees();
+    println!("Num Trees for 3x1 slope: {}", num_trees);
+
+    Ok(())
+}
+
+// ------------------------------------------------------------------------------
+// Map
+// ------------------------------------------------------------------------------
+
+#[derive(Debug)]
+struct Map(pub Vec<Row>);
+
+impl Map {
+    fn count_trees(&self) -> u32 {
+        let target_depth = self.0.len();
+        let slope = (3, 1);
+
+        let mut num_trees = 0;
+        let mut position = (0, 0);
+
+        while position.1 < target_depth {
+            let row = &self.0[position.1].0;
+            let row_length = row.len();
+
+            if row[position.0] == Tile::Tree {
+                num_trees += 1;
+            }
+
+            position.0 = (position.0 + slope.0) % row_length;
+            position.1 += slope.1;
+        }
+
+        num_trees
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        for row in &self.0 {
+            row.fmt(f)?;
+            f.write_char('\n')?;
+        }
+
+        Ok(())
+    }
+}
+
+impl FromStr for Map {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rows = s
+            .lines()
+            .map(FromStr::from_str)
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Map(rows))
+    }
+}
+
+// ------------------------------------------------------------------------------
+// Row
+// ------------------------------------------------------------------------------
+
+#[derive(Debug)]
+struct Row(pub Vec<Tile>);
+
+impl Display for Row {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        self.0.iter().map(|tile| tile.fmt(f)).collect()
+    }
+}
+
+impl FromStr for Row {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let chars = s.trim().split("");
+        let tiles = chars
+            .into_iter()
+            .map(FromStr::from_str)
+            .filter(Result::is_ok)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Row(tiles))
+    }
+}
+
+// ------------------------------------------------------------------------------
+// Tile
+// ------------------------------------------------------------------------------
+
+#[derive(Eq, PartialEq, Debug)]
+enum Tile {
+    Empty,
+    Tree,
+}
+
+impl Display for Tile {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Tile::Empty => f.write_char('.'),
+            Tile::Tree => f.write_char('#'),
+        }
+    }
+}
+
+impl FromStr for Tile {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "." => Ok(Self::Empty),
+            "#" => Ok(Self::Tree),
+            _ => Err("Unknown Tile".into()),
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------
+// Utility
+// ------------------------------------------------------------------------------
+
+fn parse_input<ResultT>() -> Result<Vec<ResultT>, Error>
+where
+    ResultT: std::str::FromStr<Err = Error>,
+{
+    let input_file = env::args().nth(1).expect("input file name missing");
+
+    fs::read_to_string(input_file)?
+        .lines()
+        .map(str::parse)
+        .collect::<Result<_, _>>()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn count_trees_in_3_1_slope() -> Result<(), Error> {
+        let map = r#"..##.......
+                          #...#...#..
+                          .#....#..#.
+                          ..#.#...#.#
+                          .#...##..#.
+                          ..#.##.....
+                          .#.#.#....#
+                          .#........#
+                          #.##...#...
+                          #...##....#
+                          .#..#...#.#"#;
+
+        let map = Map::from_str(map)?;
+        assert_eq!(7, map.count_trees());
+
+        Ok(())
+    }
+}
