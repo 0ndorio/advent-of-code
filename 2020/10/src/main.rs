93e4c3db85c26d5ab04672c2a0c2c990afe3c4d3
Bruno Kirschner <bruno.kirschner@online.de>
2020-12-12 21:54:15 +0100

    [2020|Rust] Add solution for the second part of day 10.

--- 2020/10/src/main.rs
+++ 2020/10/src/main.rs
@@ -6,6 +6,9 @@ fn main() -> Result<(), Error> {
     let joltage_differences = PowerSupply::calc_joltage_differences(&adapters);
     println!("Joltage Differences: {:#?}", joltage_differences);
 
+    let num_combinations = PowerSupply::determine_num_arrangements(&adapters);
+    println!("Num Combinations: {:#?}", num_combinations);
+
     Ok(())
 }
 
@@ -23,25 +26,59 @@ impl PowerSupply {
         }
     }
 
-    fn calc_joltage_differences(unsorted_adapters: impl AsRef<[u32]>) -> (u32, u32, u32) {
+    fn generate_joltage_differences(unsorted_adapters: impl AsRef<[u32]>) -> Vec<u32> {
         let device_joltage = Self::find_device_joltage_rating(&unsorted_adapters);
 
         let mut adapters = vec![0, device_joltage];
         adapters.extend(unsorted_adapters.as_ref());
         adapters.sort_unstable();
 
-        let mut joltage_differences = (0, 0, 0);
+        let mut joltage_differences = vec![];
         for index in 0..adapters.len() - 1 {
             let difference = adapters[index + 1] - adapters[index];
+            joltage_differences.push(difference);
+        }
+
+        joltage_differences
+    }
+
+    fn calc_joltage_differences(unsorted_adapters: impl AsRef<[u32]>) -> (u32, u32, u32) {
+        let joltage_differences = PowerSupply::generate_joltage_differences(unsorted_adapters);
+
+        let mut num_differences = (0, 0, 0);
+        for difference in joltage_differences {
             match difference {
-                1 => joltage_differences.0 += 1,
-                2 => joltage_differences.1 += 1,
-                3 => joltage_differences.2 += 1,
+                1 => num_differences.0 += 1,
+                2 => num_differences.1 += 1,
+                3 => num_differences.2 += 1,
                 _ => continue,
             }
         }
 
-        joltage_differences
+        num_differences
+    }
+
+    fn determine_num_arrangements(adapter: impl AsRef<[u32]>) -> u128 {
+        let adapter = adapter.as_ref();
+
+        let differences = PowerSupply::generate_joltage_differences(&adapter);
+        let num_differences = differences.len();
+
+        let mut combinations: Vec<u128> = vec![1; num_differences];
+
+        for index in (0..num_differences - 1).rev() {
+            let num_combinations = if differences[index] == 3 || differences[index + 1] == 3 {
+                combinations[index + 1]
+            } else if differences[index + 2] == 3 {
+                combinations[index + 1] + combinations[index + 2]
+            } else {
+                combinations[index + 1] + combinations[index + 2] + combinations[index + 3]
+            };
+
+            combinations[index] = num_combinations;
+        }
+
+        combinations[0]
     }
 }
 
@@ -86,6 +123,9 @@ mod tests {
         let joltage_differences = PowerSupply::calc_joltage_differences(&adapters);
         assert_eq!((7, 0, 5), joltage_differences);
 
+        let num_arrangements = PowerSupply::determine_num_arrangements(&adapters);
+        assert_eq!(8, num_arrangements);
+
         Ok(())
     }
 
@@ -101,6 +141,9 @@ mod tests {
 
         let joltage_differences = PowerSupply::calc_joltage_differences(&adapters);
         assert_eq!((22, 0, 10), joltage_differences);
+
+        let num_arrangements = PowerSupply::determine_num_arrangements(&adapters);
+        assert_eq!(19208, num_arrangements);
 
         Ok(())
     }
